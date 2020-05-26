b8df52ba22bd8aa24937f442d5edf8c5b60b627a
Gitau Harrison <harrisongitau3@gmail.com>
2020-05-26 18:00:26 +0300

    Add follow and unfollow link on user profile page

--- app/routes.py
+++ app/routes.py
@@ -69,7 +69,6 @@ def user(username):
         {'author': user, 'body': "Test post #1"},
         {'author': user, 'body': 'Test post #2'}
     ]
-    return render_template('user.html', user=user, posts=posts)
     form = emptyForm()
     return render_template('user.html', user = user, form = form, posts = posts)
 
@@ -94,22 +93,22 @@ def edit_profile():
         form.about_me.data = current_user.about_me
     return render_template('edit_profile.html', title = 'Edit Profile', form = form)
 
-app.route('/follow/<username>', methods = ['POST'])
+@app.route('/follow/<username>', methods=['POST'])
 @login_required
 def follow(username):
     form = emptyForm()
     if form.validate_on_submit():
-        user = User.query.filter_by(username = username).first()
+        user = User.query.filter_by(username=username).first()
         if user is None:
             flash('User {} not found.'.format(username))
             return redirect(url_for('index'))
         if user == current_user:
             flash('You cannot follow yourself!')
-            return redirect(url_for('user', username = username))
+            return redirect(url_for('user', username=username))
         current_user.follow(user)
         db.session.commit()
-        flash('You are following {}'.format(username))
-        return redirect(url_for('user', username = username))
+        flash('You are following {}!'.format(username))
+        return redirect(url_for('user', username=username))
     else:
         return redirect(url_for('index'))
 
