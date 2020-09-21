f90cca53e2e6429c72ba02d9432cea54533fb68c
Gitau Harrison <harrisongitau3@gmail.com>
2020-09-21 18:14:24 +0300

    Add login page, modify nav bar

--- app/__init__.py
+++ app/__init__.py
@@ -1,5 +1,9 @@
 from flask import Flask
+from config import Config
+from flask_bootstrap import Bootstrap
 
 app = Flask(__name__)
+app.config.from_object(Config)
+boostrap = Bootstrap(app)
 
 from app import routes, models
--- app/routes.py
+++ app/routes.py
@@ -1,6 +1,13 @@
 from app import app
+from app.forms import LoginForm
+from flask import render_template
 
 @app.route('/')
 @app.route('/home')
 def home():
-    return 'Hello'
+    return render_template('home.html', title = 'Home')
+
+@app.route('/login', methods = ['GET', 'POST'])
+def login():
+    form = LoginForm()
+    return render_template('login.html', title = 'Home', form = form)
