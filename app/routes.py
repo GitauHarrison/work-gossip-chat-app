667123596a6fa7392d9a877f791ce345a51f585c
Gitau Harrison <harrisongitau3@gmail.com>
2020-09-21 19:02:34 +0300

    Validate user login

--- app/routes.py
+++ app/routes.py
@@ -11,7 +11,16 @@ def home():
 
 @app.route('/login', methods = ['GET', 'POST'])
 def login():
+    if current_user.is_authenticated:
+        return redirect(url_for('home'))
     form = LoginForm()
+    if form.validate_on_submit():
+        user = User.query.filter_by(username = form.username.data).first()
+        if user is None or not user.check_password(form.password.data):
+            flash('Invalid username or password')
+            return redirect(url_for('login'))
+        login_user(user)
+        return redirect(url_for('home'))
     return render_template('login.html', title = 'Home', form = form)
 
 @app.route('/register', methods = ['GET', 'POST'])
