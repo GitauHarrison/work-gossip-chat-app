c6e68bb6cd8adc2bec036bd43504ca88842e2f3d
Gitau Harrison <harrisongitau3@gmail.com>
2020-09-21 18:23:20 +0300

    Add a login manager

--- app/__init__.py
+++ app/__init__.py
@@ -1,9 +1,11 @@
 from flask import Flask
 from config import Config
 from flask_bootstrap import Bootstrap
+from flask_login import LoginManager
 
 app = Flask(__name__)
 app.config.from_object(Config)
 boostrap = Bootstrap(app)
+login = LoginManager(app)
 
 from app import routes, models
