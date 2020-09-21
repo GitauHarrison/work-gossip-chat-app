9e4b8086bb2a5a7c861b3ad014527de90e5857d4
Gitau Harrison <harrisongitau3@gmail.com>
2020-09-21 18:29:10 +0300

    Initialize database

--- app/__init__.py
+++ app/__init__.py
@@ -2,10 +2,14 @@ from flask import Flask
 from config import Config
 from flask_bootstrap import Bootstrap
 from flask_login import LoginManager
+from flask_sqlalchemy import SQLAlchemy
+from flask_migrate import Migrate
 
 app = Flask(__name__)
 app.config.from_object(Config)
 boostrap = Bootstrap(app)
 login = LoginManager(app)
+db = SQLAlchemy(app)
+migrate = Migrate(app, db)
 
 from app import routes, models
