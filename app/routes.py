297e0a0df1ae50453c255961e8d7f461aaf9e11c
Gitau Harrison <harrisongitau3@gmail.com>
2020-09-22 13:05:34 +0300

    Fix page display error in routes

--- app/routes.py
+++ app/routes.py
@@ -22,7 +22,7 @@ def home():
         db.session.commit()
         flash(_('Your post is now live!'))
         return redirect(url_for('home'))
-    page = request.args.get('page', 1, type(int))
+    page = request.args.get('page', 1, type=int)
     posts = current_user.followed_posts().paginate(
         page, app.config['POSTS_PER_PAGE'], False
     )
@@ -70,7 +70,7 @@ def register():
 def profile(username):
     user = User.query.filter_by(username = username).first_or_404()
     form = EmptyForm()
-    page = request.args.get('page', 1, type(int))
+    page = request.args.get('page', 1, type=int)
     posts = user.posts.order_by(Post.timestamp.desc()).paginate(
         page, app.config['POSTS_PER_PAGE'], False
     )
@@ -82,13 +82,13 @@ def profile(username):
 
 @app.route('/explore')
 def explore():    
-    page = request.args.get('page', 1, type(int))
+    page = request.args.get('page', 1, type=int)
     posts = Post.query.order_by(Post.timestamp.desc()).paginate(
         page, app.config['POSTS_PER_PAGE'], False
     )
-    next_url = url_for('home', page = posts.next_num) \
+    next_url = url_for('explore', page = posts.next_num) \
         if posts.has_next else None
-    prev_url = url_for('home', page = posts.prev_num) \
+    prev_url = url_for('explore', page = posts.prev_num) \
         if posts.has_prev else None
     return render_template('home.html', title = 'Explore', posts = posts.items, next_url = next_url, prev_url = prev_url)
 
