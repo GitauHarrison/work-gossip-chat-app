1118fbb6c20fdc4d99df94bb025f6c4d0cbce070
Gitau Harrison <harrisongitau3@gmail.com>
2020-09-22 23:32:47 +0300

    verify token generated

--- app/models.py
+++ app/models.py
@@ -71,6 +71,14 @@ class User( UserMixin ,db.Model):
             algorithm='HS256',
             ).decode('utf-8')
 
+    @staticmethod
+    def verify_reset_password_token(token):
+        try:
+            id = jwt.decode(token, app.config['SECRET_KEY'], algorithms=['HS256']['reset_password'])
+        except:
+            return
+        return User.query.get(id)
+
 class Post(db.Model):
     id = db.Column(db.Integer, primary_key = True)
     body = db.Column(db.String(140))
