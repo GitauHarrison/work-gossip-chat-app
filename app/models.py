352fe10e2c0350e61e3ca45e0597eb5d6ebad64c
Gitau Harrison <harrisongitau3@gmail.com>
2020-09-22 11:58:59 +0300

    Create condition for language translation

--- app/models.py
+++ app/models.py
@@ -68,6 +68,8 @@ class Post(db.Model):
     timestamp = db.Column(db.DateTime, index = True, default = datetime.utcnow)
     user_id = db.Column(db.Integer, db.ForeignKey('user.id'))
 
+    language = db.Column(db.String(5))
+
     def __repr__(self):
         return 'Post <>'.format(body)
 
--- app/routes.py
+++ app/routes.py
@@ -5,6 +5,7 @@ from flask_login import current_user, login_user, logout_user, login_required
 from app.models import User, Post
 from datetime import datetime
 from flask_babel import _, get_locale
+from guess_language import guess_language
 
 @app.route('/', methods = ['GET', 'POST'])
 @app.route('/home', methods = ['GET', 'POST'])
@@ -12,7 +13,10 @@ from flask_babel import _, get_locale
 def home():
     form = PostForm() 
     if form.validate_on_submit():
-        post = Post(body = form.post.data, author = current_user)
+        language = guess_language(form.post.data)
+        if language == 'UNKNOWN' or len(language) > 5:
+            language = ''
+        post = Post(body = form.post.data, author = current_user, language = language)
         db.session.add(post)
         db.session.commit()
         flash(_('Your post is now live!'))
