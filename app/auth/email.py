041e32a773a4f31ddfb47c4ca00e596d34a0f3b9
Gitau Harrison <harrisongitau3@gmail.com>
2020-09-24 04:54:44 +0300

    Add password reset to auth blueprint

--- app/auth/email.py
+++ app/auth/email.py
@@ -1,24 +1,12 @@
-from flask_mail import Message
-from app import mail, app
-from flask import render_template
-from threading import Thread
-
-def send_async_email(app, msg):
-    with app.app_context():
-        mail.send(msg)
-
-def send_email(subject, sender, recipients, text_body, html_body):
-    msg = Message(subject, sender = sender, recipients = recipients)
-    msg.body = text_body
-    msg.html = html_body
-    mail.send(msg)
-    Thread(target=send_async_email, args=(app, msg)).start()
+from flask import render_template, current_app
+from app.email import send_email
+from flask_babel import _
 
 def send_password_reset_email(user):
     token = user.get_reset_password_token()
     send_email(
-        '[TinkerChatApp] Reset Your Password',
-        sender = app.config['ADMINS'],
+        _('[TinkerChatApp] Reset Your Password'),
+        sender = current_app.config['ADMINS'],
         recipients=[user.email],
         text_body=render_template('email/reset_password.txt', token = token, user = user),
         html_body = render_template('email/reset_password.html', token = token, user = user)       
