d045fcef02d76d3817f0efbb1a036a4fc0790714
Gitau Harrison <harrisongitau3@gmail.com>
2020-09-24 04:09:58 +0300

    Add handler file in error blueprint package

--- app/errors/handlers.py
+++ app/errors/handlers.py
@@ -1,11 +1,12 @@
-from app import app, db
+from app import db
 from flask import render_template
+from app.errors import bp
 
-@app.errorhandler(404)
+@bp.errorhandler(404)
 def page_not_found(error):
     return render_template('404.html', title = 'Page not found'), 404
 
-@app.errorhandler(500)
+@bp.errorhandler(500)
 def internal_error(error):
     db.session.rollback()
     return render_template('500.html', title = 'Internal Error'), 500
