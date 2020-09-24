10c30ea63c0a657b1bc408d4fe8739c9a6152351
Gitau Harrison <harrisongitau3@gmail.com>
2020-09-24 05:17:53 +0300

    Register main blueprint in app

--- app/__init__.py
+++ app/__init__.py
@@ -30,6 +30,9 @@ app.register_blueprint(errors_bp)
 from app.auth import bp as auth_bp
 app.register_blueprint(auth_bp, url_prefix = '/auth')
 
+from app.main import bp as main_bp
+app.register_blueprint(main_bp, url_prefix = '/main')
+
 if not app.debug:
     if app.config['MAIL_SERVER']:
         auth = None
