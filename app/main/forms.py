1debf54cc1c045650afc7bb64bfbbd9e8b834e6f
Gitau Harrison <harrisongitau3@gmail.com>
2020-09-24 05:02:49 +0300

    Add main forms to main blueprint

---
+++ app/main/forms.py
@@ -0,0 +1,27 @@
+from flask_wtf import FlaskForm
+from wtforms import StringField, SubmitField, TextAreaField
+from wtforms.validators import DataRequired, ValidationError, Length
+from app.models import User
+from flask_babel import _, lazy_gettext as _l
+
+class EditProfileForm(FlaskForm):
+    username = StringField(_l('Username'), validators = [DataRequired()])
+    about_me = TextAreaField(_l('About Me'), validators = [DataRequired(), Length(min = 0, max = 140)])
+    submit = SubmitField(_l('Update Profile'))
+
+    def __init__(self, original_username, *args, **kwargs):
+        super(EditProfileForm, self).__init__(*args, **kwargs)
+        self.original_username = original_username
+
+    def validate_username(self, username):
+        if username.data != self.original_username:
+            user = User.query.filter_by(username=self.username.data).first()
+            if user is not None:
+                raise ValidationError(_('Please use a different username.'))
+
+class EmptyForm(FlaskForm):
+    submit = SubmitField('Submit')
+
+class PostForm(FlaskForm):
+    post = TextAreaField(_l('Say Something'), validators = [DataRequired()])
+    submit = SubmitField('Post Your Comment')
