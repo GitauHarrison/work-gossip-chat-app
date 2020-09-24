080f9f6fd8b8824ed0c2dca7fa6d0a1cee604703
Gitau Harrison <harrisongitau3@gmail.com>
2020-09-24 09:22:34 +0300

    Create application factory

--- app/__init__.py
+++ app/__init__.py
@@ -1,4 +1,4 @@
-from flask import Flask, request
+from flask import Flask, request, current_app
 from config import Config
 from flask_bootstrap import Bootstrap
 from flask_login import LoginManager
@@ -12,60 +12,72 @@ from flask_babel import Babel
 from flask_babel import lazy_gettext as _l
 from flask_mail import Mail
 
-app = Flask(__name__)
-app.config.from_object(Config)
-boostrap = Bootstrap(app)
-login = LoginManager(app)
+
+boostrap = Bootstrap()
+login = LoginManager()
 login.login_view = 'login'
 login.login_message = _l('Please log in to access this page')
-db = SQLAlchemy(app)
-migrate = Migrate(app, db)
-moment = Moment(app)
-babel = Babel(app)
-mail = Mail(app)
+db = SQLAlchemy()
+migrate = Migrate()
+moment = Moment()
+babel = Babel()
+mail = Mail()
 
-from app.errors import bp as errors_bp
-app.register_blueprint(errors_bp)
+def create_app(config_class = Config):
+    app = Flask(__name__)
+    app.config.from_object(config_class)
 
-from app.auth import bp as auth_bp
-app.register_blueprint(auth_bp, url_prefix = '/auth')
+    Bootstrap.init_app(app)
+    login.init_app(app)
+    db.init_app(app)
+    migrate.init_app(app)
+    moment.init_app(app)
+    babel.init_app(app)
+    mail.init_app(app)
 
-from app.main import bp as main_bp
-app.register_blueprint(main_bp, url_prefix = '/main')
 
-if not app.debug:
-    if app.config['MAIL_SERVER']:
-        auth = None
-        if app.config['MAIL_USERNAME' or app.config['MAIL_PASSWORD']]:
-            auth = (app.config['MAIL_USERNAME'], app.config['MAIL_PASSWORD'])
-        secure = None
-        if app.config['MAIL_USE_TLS']:
-            secure = ()
-        mail_handler = SMTPHandler(
-            mailhost=(app.config['MAIL_SERVER'], app.config['MAIL_PORT']),
-            fromaddr='no-reply@'  + app.config['MAIL_SERVER'],
-            toaddrs=app.config['ADMINS'][0],
-            subject='ChatApp Failure',
-            credentials=auth, secure=secure
-        )
-        mail_handler.setLevel(logging.ERROR)
-        app.logger.addHandler(mail_handler)
+    from app.errors import bp as errors_bp
+    app.register_blueprint(errors_bp)
 
-    if not os.path.exists('logs'):
-        os.mkdir('logs')
-    file_handler = RotatingFileHandler('logs/tinker_app.log', maxBytes=10240, backupCount=10)
-    file_handler.setFormatter(logging.Formatter(
-        '%(asctime)s %(levelname)s: %(message)s [in %(pathname)s:%(lineno)d]'
-    ))
-    file_handler.setLevel(logging.INFO)
-    app.logger.addHandler(file_handler)
+    from app.auth import bp as auth_bp
+    app.register_blueprint(auth_bp, url_prefix = '/auth')
 
-    app.logger.setLevel(logging.INFO)
-    app.logger.info('Tinker ChatApp')
+    from app.main import bp as main_bp
+    app.register_blueprint(main_bp, url_prefix = '/main')
+
+    if not app.debug and not app.testing:
+        if app.config['MAIL_SERVER']:
+            auth = None
+            if app.config['MAIL_USERNAME' or app.config['MAIL_PASSWORD']]:
+                auth = (app.config['MAIL_USERNAME'], app.config['MAIL_PASSWORD'])
+            secure = None
+            if app.config['MAIL_USE_TLS']:
+                secure = ()
+            mail_handler = SMTPHandler(
+                mailhost=(app.config['MAIL_SERVER'], app.config['MAIL_PORT']),
+                fromaddr='no-reply@'  + app.config['MAIL_SERVER'],
+                toaddrs=app.config['ADMINS'][0],
+                subject='ChatApp Failure',
+                credentials=auth, secure=secure
+            )
+            mail_handler.setLevel(logging.ERROR)
+            app.logger.addHandler(mail_handler)
+
+        if not os.path.exists('logs'):
+            os.mkdir('logs')
+        file_handler = RotatingFileHandler('logs/tinker_app.log', maxBytes=10240, backupCount=10)
+        file_handler.setFormatter(logging.Formatter(
+            '%(asctime)s %(levelname)s: %(message)s [in %(pathname)s:%(lineno)d]'
+        ))
+        file_handler.setLevel(logging.INFO)
+        app.logger.addHandler(file_handler)
+
+        app.logger.setLevel(logging.INFO)
+        app.logger.info('Tinker ChatApp')
 
 @babel.localeselector
 def get_locale():
-    #return request.accept_languages.best_match(app.config['LANGUAGES'])
-    return 'en'
+    return request.accept_languages.best_match(app.config['LANGUAGES'])
+    #return 'en'
 
 from app import routes, models, errors
