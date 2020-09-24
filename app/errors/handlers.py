ac71c6748678affd6c4156111036ff7eeda10e73
Gitau Harrison <harrisongitau3@gmail.com>
2020-09-24 09:48:01 +0300

    Fix error

--- app/errors/handlers.py
+++ app/errors/handlers.py
@@ -2,11 +2,11 @@ from app import db
 from flask import render_template
 from app.errors import bp
 
-@bp.errorhandler(404)
+@bp.app_errorhandler(404)
 def page_not_found(error):
     return render_template('errors/404.html', title = 'Page not found'), 404
 
-@bp.errorhandler(500)
+@bp.app_errorhandler(500)
 def internal_error(error):
     db.session.rollback()
     return render_template('errors/500.html', title = 'Internal Error'), 500
