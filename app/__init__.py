195dc5ab8b6df6c52b87be8ad0b4f844246e6c41
Gitau Harrison <harrisongitau3@gmail.com>
2020-09-24 17:36:13 +0300

    Remove prefix in main blueprint

--- app/__init__.py
+++ app/__init__.py
@@ -43,7 +43,7 @@ def create_app(config_class=Config):
     app.register_blueprint(auth_bp, url_prefix = '/auth')
 
     from app.main import bp as main_bp
-    app.register_blueprint(main_bp, url_prefix = '/main')
+    app.register_blueprint(main_bp)
 
     if not app.debug and not app.testing:
         if app.config['MAIL_SERVER']:
