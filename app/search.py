96049fcda4aa477ede1b5bf9e1b634cadd2cd16e
Gitau Harrison <harrisongitau3@gmail.com>
2020-09-28 02:41:38 +0300

    Remove items from full text search

--- app/search.py
+++ app/search.py
@@ -7,3 +7,8 @@ def add_to_index(index, model):
     for field in model.__searchable__:
         payload[field] = getattr(model, field)
     current_app.elasticsearch.index(index = index, id = model.id, body = payload)
+
+def remove_from_index(index, model):
+    if not current_app.elasticsearch:
+        return
+    current_app.elasticsearch.indices.delete(index = index, id = model.id)
