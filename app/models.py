0740867aa5b531125d23d35c43d6b15f97dead7e
Gitau Harrison <harrisongitau3@gmail.com>
2020-09-28 03:52:02 +0300

    Close case() function which returns actual posts when elasticsearch returns object ids

--- app/models.py
+++ app/models.py
@@ -18,8 +18,7 @@ class SearchableMixin(object):
         for i in range(len(ids)):
             when.append((ids[i], i))
         return cls.query.filter(cls.id.in_(ids)).order_by(
-            db.case(when, value = cls.id), total
-        )
+            db.case(when, value = cls.id)), total
 
     @classmethod
     def before_commit(cls, session):
