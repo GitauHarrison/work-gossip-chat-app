ffcc2f7bd63339e021664a95e15e1be10dbdc6b2
Gitau Harrison <harrisongitau3@gmail.com>
2020-10-05 03:39:56 +0300

    Fix error in verrifying token

--- app/models.py
+++ app/models.py
@@ -117,7 +117,7 @@ class User( UserMixin ,db.Model):
     @staticmethod
     def verify_reset_password_token(token):
         try:
-            id = jwt.decode(token, current_app.config['SECRET_KEY'], algorithms=['HS256']['reset_password'])
+            id = jwt.decode(token, current_app.config['SECRET_KEY'], algorithms=['HS256'])['reset_password']
         except:
             return
         return User.query.get(id)
