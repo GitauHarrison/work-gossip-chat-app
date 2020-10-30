3709e4df82a4b754f78672583bea9e74192dea42
Gitau Harrison <harrisongitau3@gmail.com>
2020-10-30 11:47:49 +0300

    Fix compile error

--- app/cli.py
+++ app/cli.py
@@ -30,7 +30,5 @@ def register(app):
     @translate.command()
     def compile():
         """Compile all languages"""
-        if os.system('pybabel extract -F babel.cfg -k _l -o messages.pot .'):
-            raise RuntimeError('extract command failed')
         if os.system('pybabel compile -d app/translations'):
             raise RuntimeError('compile command failed')
