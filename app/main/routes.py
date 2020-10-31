300b5e4b6aa127d8ea105de3a6a69f9bc965d0b0
Gitau Harrison <harrisongitau3@gmail.com>
2020-10-31 17:24:03 +0300

    Display private messages

--- app/main/routes.py
+++ app/main/routes.py
@@ -163,3 +163,19 @@ def send_message(recipient):
         flash(_('Your message has been sent'))
         return redirect(url_for('main.profile', username = recipient))
     return render_template('main/send_message.html', title = _('Send Message'), form = form, user = user)
+
+@bp.route('/messages')
+def messages():
+    current_user.last_message_read_time = datetime.utcnow()
+    db.session.commit()
+    page = request.args.get('page', type = int)
+    messages = current_user.messages_received.order_by(
+        Message.timestamp.desc()
+    ).paginate(
+        page, current_app.config['POSTS_PER_PAGE'], False
+    )
+    next_url = url_for('main.messages', page = messages.next_num) \
+        if messages.has_next else None
+    prev_url = url_for('main.messages', page = messages.prev_num) \
+        if messages.has_prev else None
+    return render_template('main/messages.html', title = _('Private Messages'), messages=messages.items, next_url = next_url, prev_url = prev_url)
