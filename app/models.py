ca68164e014eb85ecefe791a12dfbda734a00227
Gitau Harrison <harrisongitau3@gmail.com>
2020-10-31 16:03:14 +0300

    Add database support for private messages

--- app/models.py
+++ app/models.py
@@ -65,6 +65,10 @@ class User( UserMixin ,db.Model):
     about_me = db.Column(db.String(140))
     last_seen = db.Column(db.DateTime, default = datetime.utcnow)
 
+    messages_sent = db.relationship('Message', foreign_keys='Message.sender_id', backref='author', lazy = 'dynamic')
+    messages_received = db.relationship('Message', foreign_keys='Message.recipient_id', backref='recipient', lazy='dynamic')
+    last_message_read_time = db.Column(db.DateTime)
+
     followed = db.relationship(
         'User', 
         secondary = followers,
@@ -122,6 +126,12 @@ class User( UserMixin ,db.Model):
             return
         return User.query.get(id)
 
+    def new_messages(self):
+        last_read_time = self.last_message_read_time or datetime(1900, 1, 1)
+        return Message.query.filter_by(recipient=self).filter(
+            Message.timestamp > last_read_time
+        ).count()
+
 class Post(SearchableMixin, db.Model):
     __searchable__ = ['body']
     id = db.Column(db.Integer, primary_key = True)
@@ -133,6 +143,16 @@ class Post(SearchableMixin, db.Model):
 
     def __repr__(self):
         return 'Post <>'.format(self.body)
+
+class Message(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    sender_id = db.Column(db.Integer, db.ForeignKey('user.id'))
+    recipient_id = db.Column(db.Integer, db.ForeignKey('user.id'))
+    body = db.Column(db.String(140))
+    timestamp = db.Column(db.DateTime, index = True, default=datetime.utcnow)
+
+    def __repr__(self):
+        return '<Message>{}'.format(self.body)
 
 @login.user_loader
 def load_user(id):
