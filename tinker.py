71f863f69cd3994c9cf08c945148e29f32f0b433
Gitau Harrison <harrisongitau3@gmail.com>
2020-11-01 03:45:51 +0300

    Update shell context with new models

--- tinker.py
+++ tinker.py
@@ -1,9 +1,9 @@
 from app import db, create_app, cli
-from app.models import User, Post
+from app.models import User, Post, Notification, Message
 
 app = create_app()
 cli.register(app)
 
 @app.shell_context_processor
 def make_shell_context():
-    return {'db': db, 'User': User, 'Post': Post}
+    return {'db': db, 'User': User, 'Post': Post, 'Notification': Notification, 'Message': Message}
