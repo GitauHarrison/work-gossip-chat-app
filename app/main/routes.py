732932f4612fdc942570df25bd36b68b09181d9e
Gitau Harrison <harrisongitau3@gmail.com>
2020-11-01 04:22:04 +0300

    Retrieve notifications

--- app/main/routes.py
+++ app/main/routes.py
@@ -2,7 +2,7 @@ from app import db
 from app.main.forms import EditProfileForm, EmptyForm, PostForm, SearchForm, MessageForm
 from flask import render_template, redirect, url_for, flash, request, g, jsonify, current_app
 from flask_login import current_user, login_required
-from app.models import User, Post, Message
+from app.models import User, Post, Message, Notification
 from datetime import datetime
 from flask_babel import _, get_locale
 from guess_language import guess_language
@@ -153,20 +153,24 @@ def search():
     return render_template('main/search.html', title = _('Search'), total = total, prev_url = prev_url, next_url = next_url, posts = posts)
 
 @bp.route('/send_message/<recipient>', methods = ['GET', 'POST'])
+@login_required
 def send_message(recipient):
     user = User.query.filter_by(username = recipient).first_or_404()
     form = MessageForm()
     if form.validate_on_submit():
         msg = Message(author = current_user, recipient=user, body  = form.message.data)
         db.session.add(msg)
+        user.add_notification('unread_message_count', user.new_messages())
         db.session.commit()
         flash(_('Your message has been sent'))
         return redirect(url_for('main.profile', username = recipient))
     return render_template('main/send_message.html', title = _('Send Message'), form = form, user = user, recipient=recipient)
 
 @bp.route('/messages')
+@login_required
 def messages():
     current_user.last_message_read_time = datetime.utcnow()
+    current_user.add_notification('unread_message_count', 0)
     db.session.commit()
     page = request.args.get('page', type = int)
     messages = current_user.messages_received.order_by(
@@ -179,3 +183,16 @@ def messages():
     prev_url = url_for('main.messages', page = messages.prev_num) \
         if messages.has_prev else None
     return render_template('main/messages.html', title = _('Private Messages'), messages=messages.items, next_url = next_url, prev_url = prev_url)
+
+@bp.route('/notifications')
+@login_required
+def notifications():
+    since = request.args.get('since', 0.0, type=float)
+    notifications = current_user.notifications.filter(
+        Notification.timestamp > since
+    ).order_by(Notification.timestamp.asc())
+    return jsonify([{
+        'name': n.name,
+        'data': n.get_data(),
+        'timestamp': n.timestamp
+    } for n in notifications])
